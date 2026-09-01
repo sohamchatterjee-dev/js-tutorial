50642b100be3612733186eb40fe2eca739a9253b
soham2002chatt <schatterjee.mohunara@gmail.com>
2026-09-01 16:16:41 +0530

    3rd

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// echo "# js-tutorial" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/sohamchatterjee-dev/js-tutorial.git
-// git push -u origin main
-
 console.log("soham");
 const accountId = 12334
 let accountEmail = "s.com"
