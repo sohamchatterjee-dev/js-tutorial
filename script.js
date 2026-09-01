32f0b5eeb297f8181e1bd30f029b5811b3f49a05
soham2002chatt <schatterjee.mohunara@gmail.com>
2026-09-01 16:14:00 +0530

    datatype

--- script.js
+++ script.js
@@ -14,3 +14,6 @@ accountCity = "kolkata"
 let accountState = "wb"
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.log(typeof accountEmail)
+
+// datatype of null- object and datatype of undefine - undefine 
